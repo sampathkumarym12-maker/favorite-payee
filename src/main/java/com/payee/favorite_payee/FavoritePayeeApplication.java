382b64e362d73c95d82f29dedfe17b0be5c1147b
SAMPATH KUMAR YM <sampathkumar@SAMPATHs-MacBook-Air.local>
2026-04-25 12:22:46 +0530

    Initial commit - favorite payee project

---
+++ src/main/java/com/payee/favorite_payee/FavoritePayeeApplication.java
@@ -0,0 +1,13 @@
+package com.payee.favorite_payee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FavoritePayeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FavoritePayeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/payee/favorite_payee/FavoritePayeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payee.favorite_payee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FavoritePayeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
